04a99307a3d02a65a3b1d36ada0d1e53dab116ac
Harmanhrm <Harmandeeephrm@gmail.com>
2024-09-02 12:23:30 +1000

    Updated to render room within layout.tsx instead of page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
 
 export default function Page() {
   return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
+  
+      <h1 className="font-5xl text-white">UI Designer</h1>
+    
   );
 }
